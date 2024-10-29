ce5c81d8f325564371251d4adc4519eac79a5c62
Amen <135021180+AmenV@users.noreply.github.com>
2024-10-29 12:53:22 +0300

    Update plot.py
    destroy widgets update

--- budget-accounting/plot.py
+++ budget-accounting/plot.py
@@ -9,15 +9,15 @@ from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationTool
 
 connect = Database.create_connection('1.sqlite')                               #запрос к подключению к БД
 class plotting():
-    def __init__(window):                                                      #Создание текстового сообщения, поля ввода и кнопки
+    def __init__(window, frame):                                               #Создание текстового сообщения, поля ввода и кнопки
         global entry_name
-        txt = Label(window, text = 'Введите id пользователя, чей график будет выведен', font = ('Times New Roman', 14)).place(x = 5, y = 100)
-        entry_name = Entry(window, width = 15)
+        txt = Label(frame, text = 'Введите id пользователя, чей график будет выведен', font = ('Times New Roman', 14)).place(x = 5, y = 100)
+        entry_name = Entry(frame, width = 15)
         entry_name.place(x = 5, y = 125)
-        btn_plot = Button(window, text = 'Построить график', command = lambda window = window: plotting.main(window))
-        btn_plot.place(x = 250, y = 200)
+        btn_plot = Button(frame, text = 'Построить график', command = lambda frame = frame: plotting.main(window))
+        btn_plot.place(x = 250, y = 150)
         
-    def main(window):
+    def main(frame):
         purchase = []
         date = []
         name = []
@@ -36,6 +36,6 @@ class plotting():
             
             plot1.bar(date, purchase)                                          #Построение графика
             plot1.legend(array2[0])                                            #Надпись
-            canvas = FigureCanvasTkAgg(fig, master=window)                     #Создание полотна и размещение фигуры в окне
+            canvas = FigureCanvasTkAgg(fig, master=frame)                      #Создание полотна и размещение фигуры в окне
             canvas.draw()                                                      #Отрисовка полотна
-            canvas.get_tk_widget().pack(side = TOP, expand = True)             #Размещение полотна
+            canvas.get_tk_widget().place(x = 0, y = 200)                       #Размещение полотна
